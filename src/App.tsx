192fa19527a3ab7f6525489dbbecaf23551d47ef
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-08-30 12:01:35 -0300

    build: :tada: Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        element={
+          <h1
+            className="text-3xl font-bold underline bg-amber-500"
+          >
+            Hello world!
+          </h1>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
